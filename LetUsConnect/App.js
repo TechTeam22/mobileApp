806c340b848c7465c08da69c237f4254679b6ba5
Koppula Rohith <156028904+koppulrh@users.noreply.github.com>
2024-04-17 19:11:21 -0400

    helo

--- LetUsConnect/App.js
+++ LetUsConnect/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from '.screens/HomeScreen';
-import LoginScreen from '.screens/LoginScreen';
-import RegisterScreen from '.screens/RegisterScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import DetailsScreen from './screens/DetailsScreen';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Overview' }} />
+        <Stack.Screen name="Details" component= {DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+export default App;
